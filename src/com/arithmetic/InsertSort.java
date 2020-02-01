32e8aad9196f1e0cf3edc5ec93bea4c0cbd91b7f
SuperChild <46644987+cxposition@users.noreply.github.com>
2020-02-01 23:49:08 +0800

    实现插入排序算法，代码思路还有点模糊

---
+++ src/com/arithmetic/InsertSort.java
@@ -0,0 +1,33 @@
+package com.arithmetic;
+
+/**
+ * @author cx
+ * @Time 2020/2/1 22:04
+ * @Description 插入排序算法实现
+ */
+public class InsertSort {
+    public static void insertSort(int[] array)
+    {
+        /**外循环遍历无须序列*/
+        for (int j = 1; j < array.length; j++)
+        {
+            /**第一步保存待插入元素的数值*/
+            int temp = array[j];
+            /**内循环遍历有序序列*/
+            for (int i = j-1; i >= 0 && temp < array[i] ; i--)
+            {
+                array[i+1] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        int[] a = {1,9,38,23,56,10,189,47,25,100};
+        insertSort(a);
+        for (int i = 0; i < a.length; i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
